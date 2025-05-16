588dba8e6a4927c9c31215f10adb7db0f2a9b91d
AymanBD <aaeb1988@gmail.com>
2025-05-16 12:41:21 +0300

    - Mark all the old segments view with _old, add the proper DivKit dependencies. - Add additional factory and handler classes to manage the DivKit configrations. - update the main ui Views [Activation View - Preview -Details] with the appropraite impl the match the DivKit configs.

--- SourceSyncSDK/Package.swift
+++ SourceSyncSDK/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7.1
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,10 +13,16 @@ let package = Package(
             name: "SourceSyncSDK",
             targets: ["SourceSyncSDK"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/divkit/divkit-ios.git", from: "31.14.0"),
+    ],
     targets: [
         .target(
             name: "SourceSyncSDK",
-            dependencies: [],
+            dependencies: [
+                .product(name: "DivKit", package: "divkit-ios"),
+                .product(name: "DivKitExtensions", package: "divkit-ios" )
+            ],
             swiftSettings: [
                 .define("SUPPORT_MULTIPLE_ARCHITECTURES")
             ]
