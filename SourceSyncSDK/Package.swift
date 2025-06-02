9918878eda00fa9c736fd990c044bdd11985bf7a
AymanBD <aaeb1988@gmail.com>
2025-06-02 11:26:39 +0300

    - update DivKit to version "32.1.0" - update the Package swift content to resolve the ARM64 support for the DivKit. - Update the iOS UI SDK README file content

--- SourceSyncSDK/Package.swift
+++ SourceSyncSDK/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SourceSyncSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/divkit/divkit-ios.git", from: "31.14.0"),
+        .package(url: "https://github.com/divkit/divkit-ios.git", from: "32.1.0"),
     ],
     targets: [
         .target(
@@ -20,7 +20,23 @@ let package = Package(
             dependencies: [
                 .product(name: "DivKit", package: "divkit-ios"),
                 .product(name: "DivKitExtensions", package: "divkit-ios")
+            ],
+            cSettings: [
+                .define("SWIFT_PACKAGE"),
+                .headerSearchPath("include")
+            ],
+            swiftSettings: [
+                .define("SWIFT_PACKAGE"),
+                .unsafeFlags([
+                    "-Xfrontend", "-enable-library-evolution"
+                ], .when(configuration: .release))
+            ],
+            linkerSettings: [
+                .linkedFramework("Foundation"),
+                .linkedFramework("UIKit")
             ]
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5],
+    cxxLanguageStandard: .cxx17
 )
